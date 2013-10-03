ea3d3c528ff4fdbcd6904e6d3f6bb87894919dd9
Xiaoxiao Liu <xiaoxiao.liu@kitware.com>
2013-10-03 01:31:25 -0400

    ENH: Center and scale tree heatmap.

--- vtk-phylo-app/vtk_tree_heatmap.py
+++ vtk-phylo-app/vtk_tree_heatmap.py
@@ -7,6 +7,7 @@ The initialize() function will be invoked by the VTKWeb framework in the
 Protocol class's own initialize() method.  The VTK application logic goes here."""
 
 import vtk
+import math
 
 def add_arguments(parser):
     parser.add_argument("--tree", help="path to phy tree file", dest="tree")
@@ -56,6 +57,43 @@ def initialize(self, VTKWebApp, args):
 
         iren.Initialize()
         view.GetRenderWindow().Render()
+
+        # adjust zoom so the item nicely fills the screen
+        itemSize = [0, 0]
+        treeHeatmapItem.GetSize(itemSize)
+
+        itemSize.append(0)
+        transformItem.GetTransform().MultiplyPoint(itemSize, itemSize)
+
+        newWidth = view.GetScene().GetSceneWidth()
+        newHeight = view.GetScene().GetSceneHeight()
+
+        sx = newWidth * 0.9 / itemSize[0]
+        sy = newHeight * 0.9 / itemSize[1]
+
+        dx = math.fabs(sx - 1.0)
+        dy = math.fabs(sy - 1.0)
+
+        if dy > dx:
+          transformItem.Scale(sy, sy)
+        else:
+          transformItem.Scale(sx, sx)
+
+        # center the item within the screen
+        itemCenter = [0, 0]
+        treeHeatmapItem.GetCenter(itemCenter)
+        itemCenter.append(0)
+
+        centerPt = vtk.vtkPoints2D()
+        centerPt.InsertNextPoint(newWidth / 2.0, newHeight / 2.0)
+        transformItem.GetTransform().InverseTransformPoints(centerPt, centerPt)
+        sceneCenter = [0, 0]
+        centerPt.GetPoint(0, sceneCenter)
+
+        dx = -1 * (itemCenter[0] - sceneCenter[0])
+        dy = -1 * (itemCenter[1] - sceneCenter[1])
+
+        transformItem.Translate(dx, dy)
 
         # VTK Web application specific
         VTKWebApp.view = view.GetRenderWindow()
