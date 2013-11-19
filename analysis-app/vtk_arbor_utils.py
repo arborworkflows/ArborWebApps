4a84f00c0ae1c98cc4d1dee597b1f601de434834
Zack Galbreath <zack.galbreath@kitware.com>
2013-11-19 13:26:33 -0500

    encode vtk as binary
    Update vtk_arbor_utils to serialize/deserialize VTK objects
    as binary, base 64 encoded strings.  This matches the format
    expected & returned by the remoter analysis server.

--- analysis-app/vtk_arbor_utils.py
+++ analysis-app/vtk_arbor_utils.py
@@ -1,40 +1,56 @@
 #!/usr/bin/env python
 
+import base64
+import os
 import tempfile
 import vtk
 
 def SerializeVTKTable(table):
+  tmp = tempfile.mktemp()
   writer = vtk.vtkTableWriter()
-  writer.SetWriteToOutputString(1)
+  writer.SetFileName(tmp)
+  writer.SetFileTypeToBinary()
   writer.SetInputData(table)
   writer.Update()
-  return writer.GetOutputString()
+  with open(tmp, 'r') as fp:
+    data = fp.read()
+  os.remove(tmp)
+  data = base64.b64encode(data)
+  return data
 
 def DeserializeVTKTable(tableString):
+  data_tableString = base64.b64decode(tableString)
   reader = vtk.vtkTableReader()
   reader.ReadFromInputStringOn()
-  reader.SetInputString(tableString)
+  reader.SetBinaryInputString(data_tableString, len(data_tableString))
   reader.Update()
   return reader.GetOutput()
 
 def SerializeVTKTree(tree):
+  tmp = tempfile.mktemp()
   writer = vtk.vtkTreeWriter()
-  writer.SetWriteToOutputString(1)
+  writer.SetFileName(tmp)
+  writer.SetFileTypeToBinary()
   writer.SetInputData(tree)
   writer.Update()
-  return writer.GetOutputString()
+  with open(tmp, 'r') as fp:
+    data = fp.read()
+  os.remove(tmp)
+  data = base64.b64encode(data)
+  return data
 
 def DeserializeVTKTree(treeString):
+  data_treeString = base64.b64decode(treeString)
   reader = vtk.vtkTreeReader()
   reader.ReadFromInputStringOn()
-  reader.SetInputString(treeString)
+  reader.SetBinaryInputString(data_treeString, len(data_treeString))
   reader.Update()
   return reader.GetOutput()
 
 def NewickToVTKTree(newick):
   reader = vtk.vtkNewickTreeReader()
   reader.SetReadFromInputString(1)
-  reader.SetInputString(newick)
+  reader.SetInputString(newick, len(newick))
   reader.Update()
   return reader.GetOutput()
 
@@ -48,7 +64,7 @@ def VTKTreeToNewick(tree):
 def CSVToVTKTable(csv):
   reader = vtk.vtkDelimitedTextReader()
   reader.SetReadFromInputString(1)
-  reader.SetInputString(csv)
+  reader.SetInputString(csv, len(csv))
   reader.SetHaveHeaders(1)
   reader.DetectNumericColumnsOn()
   reader.Update()
