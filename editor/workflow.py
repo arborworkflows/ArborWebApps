dbcc44c3b9c965e631bc02019585b454303a2bad
Jeffrey Baumes <jeff.baumes@kitware.com>
2014-01-30 17:03:35 -0500

    Adding missing workflow.py

---
+++ editor/workflow.py
@@ -0,0 +1,38 @@
+import tangelo
+import cherrypy
+import json
+import pymongo
+import bson.objectid
+import bson.json_util
+
+@tangelo.restful
+def put(id):
+	coll = pymongo.Connection("mongo")["arbor"]["workflows"]
+	doc = json.load(cherrypy.request.body)
+	doc["_id"] = bson.objectid.ObjectId(id)
+	coll.save(doc)
+	return json.dumps("updated")
+
+@tangelo.restful
+def post():
+	coll = pymongo.Connection("mongo")["arbor"]["workflows"]
+	doc = json.load(cherrypy.request.body)
+	coll.save(doc)
+	return json.dumps(str(doc["_id"]))
+
+@tangelo.restful
+def get(id = None):
+	coll = pymongo.Connection("mongo")["arbor"]["workflows"]
+	if id == None:
+		return [{"id": str(doc["_id"]), "name": doc["name"]} for doc in coll.find(fields=["name"])]
+	doc = coll.find_one(bson.objectid.ObjectId(id))
+	if doc:
+		del doc["_id"]
+		return json.dumps(doc, default=bson.json_util.default)
+	return tangelo.HTTPStatusCode(404)
+
+@tangelo.restful
+def delete(id):
+	coll = pymongo.Connection("mongo")["arbor"]["workflows"]
+	doc = coll.remove(bson.objectid.ObjectId(id))
+	return json.dumps("deleted")
