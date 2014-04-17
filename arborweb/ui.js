db3e01918a2a28acbfa59bc0a2443e390c076756
Curtis Lisle <clisle@knowledgevis.com>
2014-04-16 21:52:09 -0400

    adding a .newick format file as a tree recognized as newick type.

--- arborweb/ui.js
+++ arborweb/ui.js
@@ -94,6 +94,7 @@ $(document).ready(function () {
             "table:nested.json": "nested-json",
             "tree:nexus": "nex",
             "tree:newick": "phy",
+             "tree:newick": "newick",
             "image:png": "png",
             "r:serialized": "rds"
         },
