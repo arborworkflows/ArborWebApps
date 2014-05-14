acf39312533ab1af71b14d4a8a3751156fdff3c4
Zack Galbreath <zack.galbreath@kitware.com>
2014-05-14 17:13:34 -0400

    rename simple
    putting this project on the back burner for now while we
    investigate ancestral states.

--- fitContinuous/js/main.js
+++ fitContinuous/js/main.js
@@ -6,7 +6,13 @@
     $(document).ready(function () {
         girder.apiRoot = '/girder/api/v1';
         var app = new flow.App();
+        app.fitContinuousId = "536d2f2d2028ea64be83bd26";
 
+        app.readyToAnalyze = function () {
+            if ("column" in this && "table" in this && "tree" in this) {
+                d3.select("#analyze").classed('hidden', false)
+            }
+        }
 
         // override upload function for simple mode
         app.datasetsView.upload = function (file) {
@@ -26,6 +32,7 @@
                 // modifications for simple app begin here
                 // if its a table, get the column names
                 if (flow.extensionToType[extension].type == "table") {
+                    app.table = dataset.get('data');
                     d3.select("#table-name").html('table: ' + file.name + ' <span class="glyphicon glyphicon-ok-circle"></span>');
                     $("#column-input").text("Parsing column names...");
                     flow.retrieveDatasetAsFormat(dataset, "table", "column.names", false, _.bind(function (error, dataset) {
@@ -41,8 +48,10 @@
                     }, this));
                 }
                 else if (flow.extensionToType[extension].type == "tree") {
+                    app.tree = dataset.get('data');
                     d3.select("#tree-name").html('tree: ' + file.name + ' <span class="glyphicon glyphicon-ok-circle"></span>');
                 }
+                app.readyToAnalyze();
 
                 this.datasets.off('add', null, 'set-collection').add(dataset);
             }, this);
@@ -53,11 +62,13 @@
         $("#column-input").droppable({
             drop: function( event, ui ) {
                 var COI = ui.draggable.text();
+                app.column = COI;
                 d3.select("#column-input")
                     .classed('btn-primary', true)
                     .classed('btn-success', false)
                     .classed('bg-warning', false)
                     .html(COI + ' <span class="glyphicon glyphicon-ok-circle"></span>');
+                app.readyToAnalyze();
             },
             over: function (event, ui) {
                 d3.select("#column-input")
@@ -71,7 +82,57 @@
             }
             });
 
+        $("#analyze").click(function() {
+            $("#analyze").attr("disabled", "disabled");
+            $("#analyze").text("Re-run");
+            $("#notice").text("Performing fit continuous analysis...");
 
+            var inputs = {
+                table: {type: "table", format: "csv", data: app.table},
+                tree: {type: "tree", format: "newick", data: app.tree},
+                column: {type: "string", format: "text", data: app.column},
+                model: {type: "string", format: "text", data: "EB"}
+            };
+
+            var outputs = {
+                result: {type: "tree", format: "newick"},
+                fit: {type: "table", format: "rows"}
+            };
+
+            flow.performAnalysis(app.fitContinuousId, inputs, outputs,
+                _.bind(function (error, result) {
+                    app.taskId = result.id;
+                    setTimeout(_.bind(app.checkFitContinuousResult, app), 1000);
+                }, app));
+
+        app.checkFitContinuousResult = function () {
+            d3.json(girder.apiRoot + '/item/' + this.fitContinuousId + '/romanesco/' + this.taskId + '/status', _.bind(function (error, result) {
+                console.log(result.status);
+                if (result.status === 'SUCCESS') {
+                    // get result data
+                    d3.json(girder.apiRoot + '/item/' + this.fitContinuousId + '/romanesco/' + this.taskId + '/result', _.bind(function (error, data) {
+                        app.fitTable = data.result.fit.data;
+                        app.fitTree = data.result.result.data;
+
+                        // render fit table
+                        $("#fit_table").table({ data: app.fitTable });
+
+                        // perform tree comparison
+                        $("#analyze").removeAttr("disabled");
+                        $("#notice").text("Fit continuous succeeded!  Comparing results to input...");
+                    }, this));
+
+                } else if (result.status === 'FAILURE') {
+                    $("#analyze").removeAttr("disabled");
+                    $("#notice").text("Analysis failed. " + result.message);
+                } else {
+                    setTimeout(_.bind(this.checkFitContinuousResult, this), 1000);
+                }
+            }, this));
+        }
+
+
+        });
 
         app.render();
     });
