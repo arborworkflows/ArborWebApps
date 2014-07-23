2409f9b692bc19f0c1d56b38ab8851396c9361fe
Jeffrey Baumes <jeff.baumes@kitware.com>
2014-07-23 13:36:07 -0400

    Easy mode phylogenetic signal

---
+++ phylogenetic-signal/js/main.js
@@ -0,0 +1,213 @@
+/*jslint browser: true, nomen: true */
+
+(function (flow, $, girder) {
+    'use strict';
+
+    $(document).ready(function () {
+        girder.apiRoot = '/girder/api/v1';
+        var app = new flow.App();
+
+        // arbor.kitware.com
+        //app.analysisId = "5336cd85d591e43b17a7c270";
+
+        // Jeff's system
+        app.analysisId = "53cfefe7358ebfb5e9bae0e4";
+
+        app.readyToAnalyze = function () {
+            if ("column" in this && "table" in this && "tree" in this) {
+                d3.select("#analyze").classed('disabled', false);
+            }
+        };
+
+        function toggleInputTablePreview() {
+            if ($("#table-preview-icon").hasClass("glyphicon-folder-close")) {
+                $("#table-preview-icon").removeClass("glyphicon-folder-close");
+                $("#table-preview-icon").addClass("glyphicon-folder-open");
+                $("#table-preview-text").text(" Hide input table preview");
+            }
+            else {
+                $("#table-preview-icon").removeClass("glyphicon-folder-open");
+                $("#table-preview-icon").addClass("glyphicon-folder-close");
+                $("#table-preview-text").text(" Show input table preview");
+            }
+
+            $("#input-table-vis").toggle('slow');
+        }
+
+        // override upload function for simple mode
+        app.datasetsView.upload = function (file) {
+            var reader = new FileReader();
+
+            reader.onload = _.bind(function (e) {
+                var dataset = {
+                        name: file.name,
+                        data: e.target.result
+                    },
+                    extension = file.name.split('.');
+
+                extension = extension[extension.length - 1];
+                _.extend(dataset, flow.extensionToType[extension]);
+                dataset = new Backbone.Model(dataset);
+
+                // modifications for simple app begin here
+                // if its a table, get the column names
+                if (flow.extensionToType[extension].type == "table") {
+                    app.table = dataset.get('data');
+                    app.tableFormat = flow.extensionToType[extension].format;
+                    d3.select("#table-name").html('Table: ' + file.name + ' <span class="glyphicon glyphicon-ok-circle"></span>');
+                    $("#column-input").text("Parsing column names...");
+                    $("#column-names").empty();
+                    flow.retrieveDatasetAsFormat(dataset, "table", "column.names.discrete", false, _.bind(function (error, dataset) {
+                        var columnNames = dataset.get('data');
+                        for (var i = 0; i < columnNames.length; ++i) {
+                            // create drag-and-drop elements here
+                            $("#column-names").append('<div class="btn btn-info draggable discrete">' + columnNames[i] + '</div>');
+                        }
+                        $(".draggable").draggable({
+                             zIndex: 1, helper: "clone"
+                        });
+                        d3.select("#column-input").html('Drag column of interest here <span class="glyphicon glyphicon-exclamation-sign"></span>');
+                    }, this));
+                    flow.retrieveDatasetAsFormat(dataset, "table", "column.names.continuous", false, _.bind(function (error, dataset) {
+                        var columnNames = dataset.get('data');
+                        for (var i = 0; i < columnNames.length; ++i) {
+                            // create drag-and-drop elements here
+                            $("#column-names").append('<div class="btn btn-info draggable continuous">' + columnNames[i] + '</div>');
+                        }
+                        $(".draggable").draggable({
+                             zIndex: 1, helper: "clone"
+                        });
+                    }, this));
+
+                    flow.retrieveDatasetAsFormat(dataset, "table", "rows", false, _.bind(function (error, dataset) {
+                      // show the input table to help the user understand if their data
+                      // was parsed correctly or not
+                      var rowData = dataset.get('data');
+                      rowData.rows = rowData.rows.slice(0, 3);
+                      d3.select("#input-table-vis-container").classed('hidden', false);
+                      $("#input-table-vis").table({ data: rowData });
+                    }, this));
+
+                }
+
+                else if (flow.extensionToType[extension].type == "tree") {
+                    app.tree = dataset.get('data');
+                    d3.select("#tree-name").html('Tree: ' + file.name + ' <span class="glyphicon glyphicon-ok-circle"></span>');
+                }
+                app.readyToAnalyze();
+
+                this.datasets.off('add', null, 'set-collection').add(dataset);
+            }, this);
+
+            reader.readAsText(file);
+        };
+
+        $("#column-input").droppable({
+            drop: function( event, ui ) {
+                var COI = ui.draggable.text();
+                app.type = "discrete";
+                if (ui.draggable.hasClass("continuous")) {
+                    app.type = "continuous";
+                }
+                app.column = COI;
+                d3.select("#column-input")
+                    .classed('btn-primary', true)
+                    .classed('btn-success', false)
+                    .classed('bg-warning', false)
+                    .html(COI + ' <span class="glyphicon glyphicon-ok-circle"></span>');
+                app.readyToAnalyze();
+            },
+            over: function (event, ui) {
+                d3.select("#column-input")
+                    .classed('btn-success', true)
+                    .classed('bg-warning', false);
+            },
+            out: function (event, ui) {
+                d3.select("#column-input")
+                    .classed('btn-success', false)
+                    .classed('bg-warning', true);
+            }
+            });
+
+        $("#analyze").click(function() {
+            $("#analyze").attr("disabled", "disabled");
+            $("#analyze").text("Re-run");
+            $("#notice").text("Performing analysis...");
+
+            var inputs = {
+                table:  {type: "table",  format: app.tableFormat,    data: app.table},
+                tree:   {type: "tree",   format: "newick",           data: app.tree},
+                column: {type: "string", format: "text",             data: app.column},
+                method: {type: "string", format: "text",             data: "lambda"}
+            };
+
+            var outputs = {
+                result: {type: "table", format: "rows"}
+            };
+
+            flow.performAnalysis(app.analysisId, inputs, outputs,
+                _.bind(function (error, result) {
+                    app.taskId = result.id;
+                    setTimeout(_.bind(app.checkResult, app), 1000);
+                }, app));
+
+        app.checkResult = function () {
+            d3.json(girder.apiRoot + '/item/' + this.analysisId + '/romanesco/' + this.taskId + '/status', _.bind(function (error, result) {
+                console.log(result.status);
+                if (result.status === 'SUCCESS') {
+                    // get result data
+                    d3.json(girder.apiRoot + '/item/' + this.analysisId + '/romanesco/' + this.taskId + '/result', _.bind(function (error, data) {
+                        app.result = data.result.result.data;
+
+                        // render table
+                        $("#result").table({ data: app.result });
+                        $("#analyze").removeAttr("disabled");
+                        $("#notice").text("Analysis succeeded!");
+                        $('html, body').animate({
+                            scrollTop: $("#result").offset().top
+                        }, 1000);
+                    }, this));
+
+                } else if (result.status === 'FAILURE') {
+                    $("#analyze").removeAttr("disabled");
+                    $("#notice").text("Analysis failed. " + result.message);
+                } else {
+                    setTimeout(_.bind(this.checkResult, this), 1000);
+                }
+            }, this));
+        };
+
+        });
+
+        $("#help").click(function() {
+            $("#upload").popover({
+                title: 'Step #1',
+                content: 'Upload your table (csv or tsv) and tree (newick) here',
+                placement: 'bottom',
+                trigger: 'manual'
+            });
+            $("#upload").popover('toggle');
+            $("#column-input").popover({
+                title: 'Step #2',
+                content: 'Drag your column of interest here',
+                placement: 'left',
+                trigger: 'manual'
+            });
+            $("#column-input").popover('toggle');
+            $("#analyze").popover({
+                title: 'Step #3',
+                content: 'Click on the "Go!" button',
+                placement: 'bottom',
+                trigger: 'manual'
+            });
+            $("#analyze").popover('toggle');
+
+        });
+
+        $("#table-preview").click(function() {
+            toggleInputTablePreview();
+        });
+
+        app.render();
+    });
+}(window.flow, window.$, window.girder));
