1918710bb7a6ce7d11a26166b9ce74ac507960d3
Dan LaManna <dan.lamanna@kitware.com>
2016-05-04 14:42:50 -0400

    Remove obsolete easy mode apps

--- arborweb/README.md
+++
@@ -1,3 +0,0 @@
-
-The code for arborweb has been moved to [tangelo-hub](https://github.com/tangelo-hub/tangelo-hub).
-
--- vtk-phylo-app/load_phylo_tree_data_2_mongo.sh
+++
@@ -1,1 +0,0 @@
-/usr/bin/mongoimport --host mongo --db arbor --collection phylotree --jsonArray --drop --file data_collection.json
