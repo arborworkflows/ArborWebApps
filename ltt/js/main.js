6149869c7475a193c29fe2b20e355556bd9c598b
Luke Harmon <lukeharmon@Lukes-MacBook-Pro.local>
2017-05-25 09:20:32 -0700

    basic ltt app

--- ltt/js/main.js
+++ ltt/js/main.js
@@ -5,10 +5,12 @@
 
     $(document).ready(function () {
         girder.apiRoot = '/girder/api/v1';
-        var app = new flow.App();
+        var app = new flow.App({
+            el: 'body'
+        });
 
         // Lookup the ID of the analysis that we wish to perform.
-        app.analysisName = "ltt_plot";
+        app.analysisName = "ltt_plot-app";
         girder.restRequest({
             path: 'resource/search',
             data: {
@@ -26,23 +28,9 @@
             }
         };
 
-        function toggleInputTablePreview() {
-            if ($("#table-preview-icon").hasClass("glyphicon-folder-close")) {
-                $("#table-preview-icon").removeClass("glyphicon-folder-close");
-                $("#table-preview-icon").addClass("glyphicon-folder-open");
-                $("#table-preview-text").text(" Hide input table preview");
-            }
-            else {
-                $("#table-preview-icon").removeClass("glyphicon-folder-open");
-                $("#table-preview-icon").addClass("glyphicon-folder-close");
-                $("#table-preview-text").text(" Show input table preview");
-            }
-
-            $("#input-table-vis").toggle('slow');
-        }
 
         // override upload function for simple mode
-        app.datasetsView.upload = function (file) {
+        flow.DatasetManagementView.prototype.upload = function (file) {
             var reader = new FileReader();
 
             reader.onload = _.bind(function (e) {
@@ -50,14 +38,16 @@
                         name: file.name,
                         data: e.target.result
                     },
-                    extension = file.name.split('.');
+                    extension = file.name.split('.'),
+                    typeFormat;
 
                 extension = extension[extension.length - 1];
-                _.extend(dataset, flow.extensionToType[extension]);
+                typeFormat = flow.getTypeFormatsFromExtension(extension)[0];
+                typeFormat = {type: typeFormat.type, format: typeFormat.format};
+                _.extend(dataset, typeFormat);
                 dataset = new Backbone.Model(dataset);
 
-
-                if (flow.extensionToType[extension].type == "tree") {
+                if (typeFormat.type == "tree") {
                     app.tree = dataset.get('data');
                     d3.select("#tree-name").html('Tree: ' + file.name + ' <span class="glyphicon glyphicon-ok-circle"></span>');
                 }
@@ -70,6 +60,7 @@
         };
 
 
+
         $("#analyze").click(function() {
             $("#analyze").attr("disabled", "disabled");
             $("#analyze").text("Re-run");
@@ -80,8 +71,7 @@
             };
 
             var outputs = {
-                lttPlot: {type: "image", format: "png.base64"},
-                lttCoords: {type: "table", format: "rows"}
+                lttPlot: {type: "image", format: "png.base64"}
             };
 
             flow.performAnalysis(app.analysisId, inputs, outputs,
@@ -97,16 +87,13 @@
                 if (result.status === 'SUCCESS') {
                     // get result data
                     var result_url = '/item/' + this.analysisId + '/flow/' + this.taskId + '/result'
-
                     girder.restRequest({path: result_url}).done(_.bind(function (data) {
                         app.lttPlot = data.result.lttPlot.data;
-                        app.lttCoords = data.result.lttCoords.data;
+
+
+
                         // render results
                         $("#ltt-plot").image({ data: app.lttPlot });
-
-
-
-                        console.log(app.lttCoords)
 
                         $("#analyze").removeAttr("disabled");
                         $("#notice").text("Analysis succeeded!");
