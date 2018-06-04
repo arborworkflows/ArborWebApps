a65b7bdd2c02e866996739d1c96d08b33d33b6ae
Luke Harmon <lukeharmon@Lukes-MacBook-Pro-2.local>
2018-06-04 15:36:23 -0700

    wat

--- discreteCorrelation/js/main.js
+++ discreteCorrelation/js/main.js
@@ -189,9 +189,7 @@
                     girder.restRequest({path: result_url}).done(_.bind(function (data) {
                         app.result = data.result.dcResultTable.data;
 
-            console.log(app.result.rows);
-						console.log(app.result.rows[0].valueOf());
-            console.log(app.result.data.rows[0]["lrStat"]);
+            console.log(app.result.rows[0]["lrStat"]);
 
 
                         // render results
